ab7c16140cdfd40794fa8c422d50d2cab51f65f7
beesman93 <chvojtomas@gmail.com>
2023-12-09 20:46:30 +0100

    concising

--- Program.cs
+++ Program.cs
@@ -1,14 +1,7 @@
-﻿using System.Diagnostics;
-using System.Text.RegularExpressions;
-
-List<string> lines = new();
+﻿List<string> lines = new();
 using (StreamReader reader = new(args[0]))
-{
     while (!reader.EndOfStream)
-    {
-        lines.Add(reader.ReadLine());
-    }
-}
+        lines.Add(reader.ReadLine()??"");
 
 solve(false);
 solve(true);
@@ -16,53 +9,37 @@ solve(true);
 void solve(bool part2)
 {
     int runTotal = 0;
-
-
-    foreach (string line in lines.Skip(0))
+    foreach (string line in lines)
     {
         int curr = 0;
-
         var hh = line.Split(' ');
 
-        List<List<int>> history = new();
-        history.Add(new List<int>());
-        foreach(var h in hh)
-            history[0].Add(int.Parse(h));
+        List<List<int>> history = new(){new()};
+        foreach (var h in hh)
+            history.First().Add(int.Parse(h));
 
-        if (part2)
-            history[0].Reverse();
-
-        int depth = 0;
+        if (part2) history[0].Reverse();
 
         bool zeroes = false;
-        while (!zeroes)
+        for(int depth = 0;!zeroes;depth++)
         {
-            history.Add(new List<int>());
+            history.Add(new());
             zeroes = true;
-            for (int i = 1; i < history[depth].Count(); i++)
+            for (int i = 1; i < history[depth].Count; i++)
             {
                 int diff = history[depth][i] - history[depth][i - 1];
-                if (diff!=0)
-                {
-                    zeroes = false;
-                }
+                if (diff!=0) zeroes = false;
                 history[depth+1].Add(diff);
             }
-            depth++;
         }
-
-        history.Last().Add(0);
 
         for (int i = history.Count - 2; i >= 0; i--)
         {
             int lastDiff = history[i].Last() + history[i + 1].Last();
             history[i].Add(lastDiff);
-            if (i == 0)
-                curr += lastDiff;
+            if (i == 0) curr += lastDiff;
         }
-        
         runTotal += curr;
     }
-
     Console.WriteLine(runTotal);
 }
